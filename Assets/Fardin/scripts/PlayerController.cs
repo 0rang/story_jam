b91df9998b07dd9b469f0c99bf0d9ba2bdb61124
Fardin Fahim <sc19ff@leeds.ac.uk>
2020-07-30 11:32:46 +0100

    haven't pushed in a while, setup for my demo

---
+++ Assets/Fardin/scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    protected Transform theTransform;
+
+    public float speed;
+
+    protected Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theTransform = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    // void move()
+    // {
+    //     switch ()
+    //     {
+
+    //         default:
+    //     }
+    //     theTransform.position.x += velocity
+    // }
+}
