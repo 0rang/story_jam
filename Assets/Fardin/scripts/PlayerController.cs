6c87675e67c347cfc87680c216e6e3b28f4a5ed8
Fardin Fahim <sc19ff@leeds.ac.uk>
2020-08-01 19:43:23 +0100

    basic movement baby!!!!!!!

--- Assets/Fardin/scripts/PlayerController.cs
+++ Assets/Fardin/scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    protected Transform theTransform;
+    Transform theTransform;
 
     public float speed;
+    private float speedDiagComponent;
 
     protected Vector2 velocity;
 
@@ -14,11 +15,30 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         theTransform = gameObject.GetComponent<Transform>();
+        speedDiagComponent = Mathf.Sqrt(0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        float inputHorizontal = Input.GetAxisRaw("Horizontal");
+        float inputVertical = Input.GetAxisRaw("Vertical");
+
+        bool movingOnBothAxes = inputHorizontal * inputVertical != 0;
+
+        if (inputHorizontal != 0 || inputVertical != 0)
+        {
+            // theTransform.position.Set(theTransform.position.x + speed * inputHorizontal * (movingOnBothAxes ? speedDiagComponent : 1),
+            // theTransform.position.y + speed * inputVertical * (movingOnBothAxes ? speedDiagComponent : 1),
+            // theTransform.position.z);
+
+            // theTransform.position.Set(theTransform.position.x + 5, theTransform.position.y + 5,
+            // theTransform.position.z);
+
+            theTransform.position = new Vector3(theTransform.position.x + speed * inputHorizontal * (movingOnBothAxes ? speedDiagComponent : 1) * Time.deltaTime,
+            theTransform.position.y + speed * inputVertical * (movingOnBothAxes ? speedDiagComponent : 1) * Time.deltaTime,
+            theTransform.position.z);
+        }
     }
 
     void FixedUpdate()
@@ -26,13 +46,8 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    // void move()
-    // {
-    //     switch ()
-    //     {
+    void move()
+    {
 
-    //         default:
-    //     }
-    //     theTransform.position.x += velocity
-    // }
+    }
 }
